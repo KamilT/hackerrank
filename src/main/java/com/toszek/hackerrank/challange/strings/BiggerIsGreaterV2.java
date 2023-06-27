c14249a2688c63f1fe29c8f7ede5f469fcc4fe01
Kamil Toszek <kamil.toszek.external@zf.com>
2023-06-27 09:15:51 +0300

    Bigger is better optimal solution

--- src/main/java/com/toszek/hackerrank/challange/strings/BiggerIsGreaterV2.java
+++ src/main/java/com/toszek/hackerrank/challange/strings/BiggerIsGreaterV2.java
@@ -1,7 +1,6 @@
 package com.toszek.hackerrank.challange.strings;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Arrays;
 
 /**
  * https://www.hackerrank.com/challenges/bigger-is-greater/problem
@@ -9,6 +8,16 @@ import java.util.TreeSet;
  * Given a word, create a new word by swapping some or all of its characters. This new word must meet two criteria:
  * It must be greater than the original word
  * It must be the smallest word that meets the first condition
+ * <p>
+ * Convert the input string to a character array to make it mutable.
+ * Start iterating from the rightmost character of the string.
+ * Find the first character that is smaller than its next character in the array. Let's call this character pivot.
+ * If no such character is found, it means the string is already the largest possible permutation. Return the input string as it is.
+ * Find the smallest character greater than pivot on its right side in the array. Let's call this character swap.
+ * Swap pivot with swap.
+ * Sort the characters to the right of pivot in ascending order.
+ * Reconstruct the resulting string from the character array.
+ * Return the modified string as the output.
  */
 public class BiggerIsGreaterV2 {
     public static final String NO_ANSWER = "no answer";
@@ -16,45 +25,40 @@ public class BiggerIsGreaterV2 {
     public static String biggerIsGreater(String w) {
         if (w.length() == 1) return NO_ANSWER;
 
-        int replacedCharIndex = 0;
-        Set<String> result = new TreeSet<>();
-        result.add(w);
-        while (replacedCharIndex < w.length() - 1) {
-            Set<String> temp = new TreeSet<>();
-            for (String word : result) {
-                for (int i = replacedCharIndex; i < w.length(); i++) {
-                    String newWord = replaceAndGet(word, i, replacedCharIndex);
-                    if (!isSmaller(w, newWord, replacedCharIndex)) {
-                        temp.add(newWord);
-                    }
-                }
-            }
-            result = temp;
-            replacedCharIndex++;
-        }
+        char[] chars = w.toCharArray();
 
-        return result.stream()
-                .filter(word -> word.compareTo(w) > 0)
-                .findFirst().orElse(NO_ANSWER);
-    }
-
-    private static boolean isSmaller(final String word, final String newWord, int upToIndex) {
-        int diff;
-        for (int i = 0; i <= upToIndex; i++) {
-            diff = newWord.charAt(i) - word.charAt(i);
-            if (diff > 0) {
-                return false;
-            } else if (diff < 0) {
-                return true;
+        // Find the pivot character
+        int pivotIndex = -1;
+        for (int i = chars.length - 2; i >= 0; i--) {
+            if (chars[i] < chars[i + 1]) {
+                pivotIndex = i;
+                break;
             }
         }
-        return false;
-    }
 
-    private static String replaceAndGet(final String word, final int i, final int k) {
-        StringBuilder sb = new StringBuilder(word);
-        sb.setCharAt(i, word.charAt(k));
-        sb.setCharAt(k, word.charAt(i));
-        return sb.toString();
+        // If no pivot found, the string is already the largest permutation
+        if (pivotIndex == -1) {
+            return NO_ANSWER;
+        }
+
+        // Find the smallest character greater than pivot on its right side
+        int swapIndex = -1;
+        for (int i = chars.length - 1; i > pivotIndex; i--) {
+            if (chars[i] > chars[pivotIndex]) {
+                swapIndex = i;
+                break;
+            }
+        }
+
+        // Swap pivot and swap characters
+        char temp = chars[pivotIndex];
+        chars[pivotIndex] = chars[swapIndex];
+        chars[swapIndex] = temp;
+
+        // Sort the characters to the right of pivot in ascending order
+        Arrays.sort(chars, pivotIndex + 1, chars.length);
+
+        // Reconstruct the resulting string
+        return new String(chars);
     }
 }
